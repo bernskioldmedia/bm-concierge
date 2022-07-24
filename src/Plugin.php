1d1faf40c2f1e05e3f984c4f0df550366aff1887
Erik Bernskiold <erik@bernskioldmedia.com>
2022-07-24 22:13:15 +0200

    Formatting
    Signed-off-by: Erik Bernskiold <erik@bernskioldmedia.com>

--- src/Plugin.php
+++ src/Plugin.php
@@ -9,9 +9,9 @@ defined( 'ABSPATH' ) || exit;
 
 class Plugin extends BasePlugin {
 
-	protected static string $slug             = 'bm_concierge';
-	protected static string $version          = '1.0.1';
-	protected static string $textdomain       = 'bm-concierge';
+	protected static string $slug = 'bm_concierge';
+	protected static string $version = '1.0.1';
+	protected static string $textdomain = 'bm-concierge';
 	protected static string $plugin_file_path = BM_CONCIERGE_FILE_PATH;
 
 	protected static array $boot = [
